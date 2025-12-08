60ca793aa6fcaba210de9674da560fc756d47d4c
Futwa, Thando <ST10443481@imconnect.edu.za>
2025-12-08 14:57:18 +0200

    updated cart to show total and edited the remove button

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 export default function CartPage() {
   const { cart, removeFromCart } = useCart();
 
+   // Calculate total amount
+  const total = cart.reduce((sum, item) => sum + item.price, 0);
+
   if (cart.length === 0)
     return <p>Your cart is empty.</p>;
 
@@ -21,12 +24,18 @@ export default function CartPage() {
           <span>R{item.price}</span>
           <button
             onClick={() => removeFromCart(item.id)}
-            className="text-red-600"
+            className="bg-red-600 text-white px-6 py-2 rounded-lg hover:bg-red-600 transition"
           >
             Remove
           </button>
         </div>
       ))}
+
+        {/* Cart Total */}
+          <div className="mt-6 p-4 bg-gray-100 rounded-lg flex justify-between items-center">
+            <span className="text-xl font-semibold">Total:</span>
+            <span className="text-2xl font-bold">R{total}</span>
+          </div>
 
       <Link href="/checkout">
         <button className="mt-6 px-6 py-3 bg-green-600 text-white rounded-lg">
